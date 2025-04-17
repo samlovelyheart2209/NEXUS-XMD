73671a319c9182b47bf46271cfeac2cea0efafc6
samlovelyheart2209 <aisamdaud2209@gmail.com>
2025-04-18 02:51:38 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,46 +5,46 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "Pkdriller~cCFn3SwA#IpGGTHZhKLqNdVzD-56X8c5bHlD8qVSNqcYhc7tObDg",
 // add your Session Id 
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
-BOT_NAME: process.env.BOT_NAME || "✦ NEXUS ✦ XMD ✦",
+BOT_NAME: process.env.BOT_NAME || "✦ Sam✦✦",
 // add bot namw here for menu
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
 CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🔥,❤️‍🩹,❤️,🩷,🧡,💛,💚,💙,🩵,💜,🤎,🖤,🤍",
 // chose custom react emojis by yourself 
-DELETE_LINKS: process.env.DELETE_LINKS || "false",
-// automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "254112192119",
+DELETE_LINKS: process.env.DELETE_LINKS || "true",
+// automatic delete links without remove member 
+OWNER_NUMBER: process.env.OWNER_NUMBER || "255616440856",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "✦ NEXUS ✦ XMD ✦",
+OWNER_NAME: process.env.OWNER_NAME || "✦ Sam✦✦",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© ✦ NEXUS ✦ XMD ✦*",
+DESCRIPTION: process.env.DESCRIPTION || "*© ✦✦sam✦*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/BKYt9f00/hanstz.jpg",
 // add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> NEXUS IS SPARKING ACTIVE AND ALIVE\n\n\nKEEP USING NEXUS XMD FROM NEXUS TECH INC⚡",
+LIVE_MSG: process.env.LIVE_MSG || "> Sam IS SPARKING ACTIVE AND ALIVE\n\n\nKEEP USING Sam FROM Sam TECH INC⚡",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
 AUTO_REACT: process.env.AUTO_REACT || "false",
 // make this true or false for auto react on all msgs
-ANTI_BAD: process.env.ANTI_BAD || "false",
+ANTI_BAD: process.env.ANTI_BAD || "true",
 // false or true for anti bad words
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
-AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "*🎉👀 Seen by NEXUS XMD 🚀🔥*",
+AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "*🎉👀 Seen by sam 🚀🔥*",
 // set the auto reply massage on status reply    
-MODE: process.env.MODE || "public",
-// make bot public-private-inbox-group 
-ANTI_LINK: process.env.ANTI_LINK || "false",
+MODE: process.env.MODE || "private",
+// make bot private-inbox-group 
+ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
-AUTO_VOICE: process.env.AUTO_VOICE || "false",
-// make true for send automatic voices
+AUTO_VOICE: process.env.AUTO_VOICE || "true",
+// make true,false for send automatic voices
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 // make true for automatic stickers 
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
@@ -55,10 +55,10 @@ OWNER_REACT: process.env.OWNER_REACT || "false",
 // make it true or fasle for only react on owner msg only 
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
 // maks true for always online 
-PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+PUBLIC_MODE: process.env.PUBLIC_MODE || "false",
 // make false if want private mod
 AUTO_TYPING: process.env.AUTO_TYPING || "true",
-// true for automatic show typing   
-AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
-// make it true for auto recoding 
+// true,false for automatic show typing   
+AUTO_RECORDING: process.env.AUTO_RECORDING || "true"
+// make it true,false for auto recoding 
 };
